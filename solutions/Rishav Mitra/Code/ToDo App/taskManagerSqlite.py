e2161ba26453665d9ab49115f22480b973f884d6
Rishav <rishavmitra10@gmail.com>
2021-03-05 01:24:22 +0530

    implemented ui for ToDo App

--- solutions/Rishav Mitra/Code/ToDo App/taskManagerSqlite.py
+++ solutions/Rishav Mitra/Code/ToDo App/taskManagerSqlite.py
@@ -1,38 +1,62 @@
 import backend
 import os
+from tkinter import *
+
+root = Tk()
+root.geometry("620x450")
+root.title("To Do App")
 
 if not os.path.exists('tasksDatabase.db'):
 	backend.createTable()
 
-while True:
-	print("\nWhat would you like to do ?")
-	print("***************************")
+def add():
+	task = newTaskEntry.get()
+	backend.addNewTask(task)
+	show()
 
-	print("1. Show all tasks")
-	print("2. Add a new task")
-	print("3. Mark a task as done")
-	print("4. Remove all tasks which are done")
-	print("5. Quit app")
-	response = input()
+def done():
+	idc = doneEntry.get()
+	backend.markTaskAsDone(idc)
+	show()
 
-	if response == '1':
-		print("id |  task  | status ")
-		print("---------------------")
-		rows = backend.showAllTasks()
-		if len(rows) == 0:
-			print("No tasks added")
-		else:
-			for row in rows:
-				print(f"{row[0]} | {row[1]} | {row[2]} ")
-	elif response == '2':
-		task = input("Enter task ")
-		backend.addNewTask(task)
-	elif response == '3':
-		idc = input("Enter id of the task you have completed ")
-		backend.markTaskAsDone(idc)
-	elif response == '4':
-		backend.removeDoneTask()
-	elif response == '5':
-		break
+def remove():
+	backend.removeDoneTask()
+	show()
+
+def show():
+	tasks.delete(1.0, END)
+	rows = backend.showAllTasks()
+	if len(rows) == 0:
+		tasks.insert("end", "No tasks added")
 	else:
-		print("Invalid option")
+		for row in rows:
+			tasks.insert("end", f"{row[0]} | {row[1]} | {row[2]} \n\n")
+
+	# tasksLabel.after(1000, show)
+
+newTaskLabel = Label(root, text = "Add a new task")
+newTaskLabel.grid(row = 0, column = 0, padx = 40)
+
+newTaskEntry = Entry(root, font = "Arial")
+newTaskEntry.grid(row = 1, column = 0, padx = 25, ipady = 5)
+
+addTaskBtn = Button(root, text = "Add", command = add)
+addTaskBtn.grid(row = 1, column = 1, ipadx = 5)
+
+tasks = Text(root, height = 20, width = 40, font = "Arial")
+tasks.grid(row = 2, column = 0, rowspan = 4, padx = 10, pady = 10)
+
+doneLabel = Label(root, text = "Enter the id of any task you've completed")
+doneLabel.grid(row = 2, column = 1)
+
+doneEntry = Entry(root, font = "Arial")
+doneEntry.grid(row = 3, column = 1)
+
+okBtn = Button(root, text = "OK", command = done)
+okBtn.grid(row = 4, column = 1)
+
+removeBtn = Button(root, text = "Remove Done Tasks", command = remove)
+removeBtn.grid(row = 5, column = 1)
+
+show()
+root.mainloop()
